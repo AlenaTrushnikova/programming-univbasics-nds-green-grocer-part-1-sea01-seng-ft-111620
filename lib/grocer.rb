784ae167aad438415b0a1bfd700270a9af04a35d
Alena Trushnikova <alena.yu.t@gmail.com>
2020-11-02 21:55:33 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -17,7 +17,7 @@ coupons
 =end
 
 
-#find_item_by_name_in_collection method simulates the act of adding items to your shopping cart. The method should be able to take the string name of a particular item and a collection of items (e.g. unconsolidated_cart array above). It should then iterate over the collection and check the value of :item in each nested hash. If a match is found, the method should return that entire hash. If an item is not found, it should return nil.
+# find_item_by_name_in_collection method simulates the act of adding items to your shopping cart. The method should be able to take the string name of a particular item and a collection of items (e.g. unconsolidated_cart array above). It should then iterate over the collection and check the value of :item in each nested hash. If a match is found, the method should return that entire hash. If an item is not found, it should return nil.
 
 def find_item_by_name_in_collection(name, grocery_shelf)
   index = 0 
@@ -30,7 +30,7 @@ def find_item_by_name_in_collection(name, grocery_shelf)
   nil
 end
 
-# This returns a new Array that represents the cart. Don't merely change `cart` (i.e. mutate) it. It's easier to return a new thing.
+# consolidate_cart method should take in a 'cart', an array of hashes of individual items and return a new array of hashes, but with each item containing a quantity. This returns a new Array that represents the cart. Don't merely change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
 def consolidate_cart(unconsolidated_car)
   
