3b26d30a9054cb9ec421a87c82863dbcab0b834a
Alena Trushnikova <alena.yu.t@gmail.com>
2020-11-02 21:39:25 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,38 +1,35 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-  index = 0
-  collection.each do |sku|
+require 'pry'
+
+=begin
+unconsolidated_cart = [
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "KALE", :price => 3.00, :clearance => false}
+]
+
+coupons
+	[
+		{:item => "AVOCADO", :num => 2, :cost => 5.00},
+		{:item => "BEER", :num => 2, :cost => 20.00},
+		{:item => "CHEESE", :num => 3, :cost => 15.00}
+	]
+=end
+
+
+
+def find_item_by_name_in_collection(name, grocery_shelf)
+  index = 0 
+  grocery_shelf.each do |sku|
     if sku[:item] === name
-      return sku 
-      index += 1
-    end  
+      return sku
+    end
   end
   nil
 end
 
-def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  reciept = []
+
+def consolidate_cart(unconsolidated_car)
   
-  cart.each do |sku|
-    look_up_item = find_item_by_name_in_collection(sku[:item], reciept)
-    if look_up_item
-      reciept_index = 0
-      reciept.each do |reciept_item|
-        if reciept_item[:item] === look_up_item[:item]
-          reciept_item[:count] += 1
-        end
-        reciept_index += 1
-      end
-    else
-      sku[:count] = 1
-      reciept << sku
-    end
-  end
-  reciept
+  
+  
 end
